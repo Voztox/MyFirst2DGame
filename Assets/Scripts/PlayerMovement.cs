eea6f487ca0bfae4305bd2bdcd2f604e1ead6883
Mert <mertfaruk.ie@gmail.com>
2024-08-26 16:30:02 +0300

    Jumping has been added that scales with the speed.
    Going to fix the error of jumping infinite.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -14,5 +14,8 @@ public class Playermovement : MonoBehaviour
     private void Update(){
         body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
 
+        if(Input.GetKey(KeyCode.Space)){
+            body.velocity = new Vector2(body.velocity.x, speed/2);
+        }
     }
 }
