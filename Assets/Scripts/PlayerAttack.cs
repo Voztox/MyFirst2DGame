e2eea0e37df0fd6ca6db287dae8dacbae9cf6054
Mert <mertfaruk.ie@gmail.com>
2024-08-28 22:30:14 +0300

    Projectiles Created
    Bug: Projectiles does not travel. Going to be fixed soon when i feel mentally better.

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -2,28 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Security.Cryptography;
 using UnityEngine;
-
-public class NewBehaviourScript : MonoBehaviour
+public class PlayerAttack : MonoBehaviour
 {
-    [SerializeField] private float attackCd;
+    [SerializeField] private float attackCooldown;
+    [SerializeField] private Transform firePoint;
+    [SerializeField] private GameObject[] fireballs;
+
     private Animator anim;
     private PlayerMovement playerMovement;
-    private float cdTimer = Mathf.Infinity;
+    private float cooldownTimer = Mathf.Infinity;
 
-    private void Awake(){
+    private void Awake()
+    {
         anim = GetComponent<Animator>();
         playerMovement = GetComponent<PlayerMovement>();
     }
 
-    private void Update () {
-        if (Input.GetMouseButton(0) && cdTimer > attackCd && playerMovement.canAttack()){
+    private void Update()
+    {
+        if (Input.GetMouseButton(0) && cooldownTimer > attackCooldown && playerMovement.canAttack())
             Attack();
-        }
 
-        cdTimer += Time.deltaTime;
+        cooldownTimer += Time.deltaTime;
     }
-    private void Attack(){
+
+    private void Attack()
+    {
         anim.SetTrigger("attack");
-        cdTimer = 0;
+        cooldownTimer = 0;
+
+        fireballs[FindFireball()].transform.position = firePoint.position;
+        fireballs[FindFireball()].GetComponent<Projectile>().SetDirection(Mathf.Sign(transform.localScale.x));
+    }
+    private int FindFireball()
+    {
+        for (int i = 0; i < fireballs.Length; i++)
+        {
+            if (!fireballs[i].activeInHierarchy)
+                return i;
+        }
+        return 0;
     }
 }
