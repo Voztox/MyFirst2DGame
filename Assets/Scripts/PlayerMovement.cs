990cbd36220f85708d1da91ed07dd1346fea2ba0
Mert <mertfaruk.ie@gmail.com>
2024-08-28 18:54:47 +0300

    Wall Touch has been added
    Previous bug has been fixed.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -31,24 +31,33 @@ public class PlayerMovement : MonoBehaviour
         else if (horizontalInput < -0.01f)
             transform.localScale = new Vector3(-1, 1, 1);
  
-        if (Input.GetKey(KeyCode.W) && isGrounded())
-            Jump();
+        
  
         //sets animation parameters
         anim.SetBool("run", horizontalInput != 0);
         anim.SetBool("grounded", isGrounded());
 
 
+        
         //wall jump cool down logic
         if(wallJumpCd < 0.2f)
-        {
+        {   
+            if (Input.GetKey(KeyCode.W) && isGrounded())
+                Jump();
+
             body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
+        
+            if(onWall() && !isGrounded())
+            {
+                body.gravityScale = 0;
+                body.velocity = Vector2.zero;
+            }
+            else 
+                body.gravityScale = 3;
         }
-        if(onWall() && !isGrounded())
-        {
-            body.gravityScale = 0;
-            body.velocity = Vector2.zero;
-        }
+        else
+        wallJumpCd += Time.deltaTime;
+
     }
  
     private void Jump()
